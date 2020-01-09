5abd545bd5e4dc352abd32c375d1d95c67c16ee2
James0618 <cjj_xjtu@163.com>
2020-01-09 23:10:02 +0800

    Atari

--- Complex_environment/Atari.py
+++ Complex_environment/Atari.py
@@ -1,2 +1,17 @@
 import sources.DQN as DQN
+import torch.nn as nn
 
+
+class Network(nn.Module):
+    def __init__(self, input_shape, output_shape):
+        super(Network, self).__init__()
+        self.conv1 = nn.Sequential(     # input_shape: 3*210*160
+            nn.Conv2d(
+                in_channels=3,     # RGB: 3 channels
+                out_channels=16,   # filters' number
+                kernel_size=3,     # kernel's size
+                stride=1,
+                padding=1
+            ),                          # output_shape: 3*210*160
+            nn.ReLU()
+        )
