db54b4f7a43d54377dd7f5a3225eb78a0055da9f
James0618 <cjj_xjtu@163.com>
2020-01-11 23:01:47 +0800

    Atari

--- Complex_environment/Atari.py
+++ Complex_environment/Atari.py
@@ -3,62 +3,34 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 import gym
+import cv2
+import numpy as np
 
 
 class Network(nn.Module):
     def __init__(self, output_shape):
         super(Network, self).__init__()
-        self.conv = nn.Sequential(      # input_shape: 3*210*160
+        self.conv = nn.Sequential(      # input_shape: 2*84*84
             nn.Conv2d(
-                in_channels=6,          # RGB: 3 channels * 2 images
-                groups=2,               # 1 state -> 2 images
+                in_channels=4,          # Gray: 1 channels * 4 images
+                groups=4,               # 1 state -> 4 images
                 out_channels=16,        # filters' number
-                kernel_size=5,          # kernel's size
-                stride=2,
-                padding=2
-            ),                          # output_shape: 16*105*80
+                kernel_size=8,          # kernel's size
+                stride=4,
+            ),                          # output_shape: 16*20*20
             nn.ReLU(),
-            nn.MaxPool2d(               # input_shape:  16*105*80
-                kernel_size=3,
-                stride=1,
-                padding=1,
-            ),                          # output_shape: 16*105*80
-            nn.Conv2d(                  # input_shape:  16*105*80
+            nn.Conv2d(                  # input_shape: 16*20*20
                 in_channels=16,
                 out_channels=32,
-                kernel_size=3,
-                stride=2,
-                padding=1
-            ),                          # output_shape: 32*53*40
+                kernel_size=4,
+                stride=2,               # output_shape: 32*9*9
+            ),
             nn.ReLU(),
-            nn.MaxPool2d(               # input_shape:  32*53*40
-                kernel_size=3,
-                stride=1,
-                padding=1,
-            ),                          # output_shape: 32*53*40
-            nn.Conv2d(                  # input_shape:  32*53*40
-                in_channels=32,
-                out_channels=64,
-                kernel_size=5,
-                stride=2,
-                padding=2
-            ),                          # output_shape: 64*27*20
-            nn.ReLU(),
-            nn.Conv2d(                  # input_shape:  64*27*20
-                in_channels=64,
-                out_channels=64,
-                kernel_size=3,
-                stride=2,
-                padding=1
-            ),                          # output_shape: 64*14*10
-            nn.ReLU()
         )
         self.fcn = nn.Sequential(
-            nn.Linear(64*14*10, 64*14*2),
+            nn.Linear(32*9*9, 256),
             nn.ReLU(),
-            nn.Linear(64*14*2, 256),
-            nn.ReLU(),
-            nn.Linear(256, output_shape),
+            nn.Linear(256, output_shape)
         )
 
     def forward(self, state):
@@ -68,13 +40,20 @@ class Network(nn.Module):
         return output
 
 
+def preprocess(ob):
+    ob = cv2.cvtColor(cv2.resize(ob, (84, 110)), cv2.COLOR_BGR2GRAY)
+    ob = ob[26:110, :]
+    ret, ob = cv2.threshold(ob, 1, 255, cv2.THRESH_BINARY)
+    return np.reshape(ob, (84, 84, 1))
+
+
 if __name__ == '__main__':
     env = gym.make("SpaceInvaders-v0")
     LEARN = True
     net = Network(env.action_space.n)
     device = torch.device("cuda:0")
     net = net.to(device)
-    agent = DQN.DQN(network=net, device=device, n_replay=1000, n_action=env.action_space.n, learning_rate=0.005,
+    agent = DQN.DQN(network=net, device=device, n_replay=5000, n_action=env.action_space.n, learning_rate=0.001,
                     learn=LEARN)
     observation = env.reset()
     observation = transforms.ToTensor()(observation).unsqueeze(0)
@@ -82,38 +61,42 @@ if __name__ == '__main__':
 
     for episode in range(2000):
         observation = env.reset()
+        observation = preprocess(observation)
         observation = transforms.ToTensor()(observation).unsqueeze(0)
         state = observation
-        reward = 0
+        total_reward = 0
         for t in range(1000):
-            if episode > 200 or LEARN is False:
+            if episode > 100 or LEARN is False:
                 env.render()
-            if t < 1:
-                observation, _, done, info = env.step(0)
-                observation = transforms.ToTensor()(observation).unsqueeze(0)   # 1*3*210*160
-                state = torch.cat((state, observation), dim=1)                  # 1*6*210*160
+            if t < 3:
+                observation, reward, done, info = env.step(0)
+                observation = preprocess(observation)
+                observation = transforms.ToTensor()(observation).unsqueeze(0)   # 1*1*210*160
+                state = torch.cat((state, observation), dim=1)                  # 1*4*210*160
+                total_reward += reward
             else:
                 state_before = state
                 action = agent.choose_action(state)
                 # print('action: {}'.format(action))
                 observation, reward, done, info = env.step(int(action))
+                observation = preprocess(observation)
                 observation = transforms.ToTensor()(observation).unsqueeze(0)
-                indices = torch.LongTensor([3, 4, 5])
+                indices = torch.LongTensor([1, 2, 3])
                 state = torch.cat((torch.index_select(state, 1, indices), observation), dim=1)  # generate next state
                 if t == 500 - 1:
                     done = True
 
                 transition = [state_before, action, reward, state]
+                total_reward += reward
                 agent.store_transition(transition=transition)
                 if done:
-                    print("Episode {}: Reward -> {}".format(episode, reward))
+                    print("Episode {}: Reward -> {}".format(episode, total_reward))
                     break
 
                 # learn when replay has enough transitions
                 if episode >= 3:
                     if t % 50 == 0 and LEARN is True:
-                        pass
-                        # agent.learn()
+                        agent.learn()
 
                 # save success params
                 if t > 400:
